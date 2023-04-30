189ccf74e9d1a571b02eb9d6f0989f54b81b9bd2
Hariprasadcm08 <hariprasadcm08@gmail.com>
2023-05-01 00:50:35 +0530

    changes in footer

--- client/myapp/src/Components/Footer.jsx
+++ client/myapp/src/Components/Footer.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import './Footer.css';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <div className="icons">
-          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer"><FaGithub size={24} /></a>
-          <a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer"><FaLinkedin size={24} /></a>
-        </div>
         <span className="text">© 2023 Website</span>
       </div>
     </footer>
